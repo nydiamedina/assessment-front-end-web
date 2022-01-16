c9da0c94dc3e27958f0c2624207884f8e4771b35
Nydia Medina <nvmedina29@gmail.com>
2022-01-16 10:10:08 -0600

    Submitting Part 2 of Assessment

--- about.js
+++ about.js
@@ -1,14 +1,18 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('The form has been submitted successfully!');
 }
 
+function handleMouseover() {
+	alert('You\'re awesome because you own a rubber ducky.');
+}
 
 let form = document.querySelector('form#contact');
+let img = document.querySelector('#rubber-duck');
 
 form.addEventListener('submit', handleSubmit);
+img.addEventListener('mouseover', handleMouseover);
+
