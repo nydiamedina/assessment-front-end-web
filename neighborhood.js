a3ff48119e0b9d90d585c81e41dc4bd3a70058d1
Nydia Medina <nvmedina29@gmail.com>
2022-01-19 11:28:39 -0600

    Submitting Extra Credit of Assessment

---
+++ neighborhood.js
@@ -0,0 +1,15 @@
+function handleRestaurantClick() {
+	const restaurants = ["Chik Chak", "Casa Mía", "Summer Restaurant", "Bin Bin", "Mesón Sur"];
+  const chosenRestaurant = Math.floor(Math.random() * restaurants.length);
+
+  restaurantMessage = `Let\'s go eat to ${restaurants[chosenRestaurant]}!`;
+  
+  const restaurantText = document.querySelector('#restaurant-text');
+  restaurantText.textContent = restaurantMessage;
+
+  alert(restaurantMessage);
+}
+
+let restaurant = document.querySelector('#restaurant');
+
+restaurant.addEventListener('click', handleRestaurantClick);
